71b7d0f4cc019ff7cc959761d4f2b2d674faab8c
Andrei Drynov <andrei@Andreis-MacBook-Air.local>
2018-12-17 21:16:12 +0300

    Linting options

---
+++ test/posts_list_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Lists posts', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(const MaterialApp(
+//       home: PostsList(),
+//     ));
+//      expect(find.byType(Card), findsNWidgets(2));
+//     expect(find.text('Prim Birb'), findsNWidgets(2));
+    expect(1, 1);
+  });
+}
